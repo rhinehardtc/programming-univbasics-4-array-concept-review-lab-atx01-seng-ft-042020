5a54dc284fcb142abe09abba189767228995a017
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-03-28 00:39:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times |i|
+  if array[i] = value_to_find ? return array[i]
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
